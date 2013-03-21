254aeecab962263dd2e228c0b127d7bb8f3604f2
Puyb <puyb@puyb.net>
2013-03-21 17:34:02 +0100

    Fix URL encoding bug for non ascii char

--- lib/utils/markup.py
+++ lib/utils/markup.py
@@ -1,5 +1,6 @@
 import re
 from htmlentitydefs import  *
+import urlparse
 
 def intEnt(m):
     m = int(m.groups(1)[0])
@@ -75,6 +76,16 @@ entitydefs2 = {}
 for key,value in entitydefs.items():
     entitydefs2[value] = key
 
+def urlEncodeNonAscii(b):
+    return re.sub('[\x80-\xFF]', lambda c: '%%%02x' % ord(c.group(0)), b)
+
+def iriToUri(iri):
+    parts = urlparse.urlparse(iri)
+    return urlparse.urlunparse(
+        part.encode('idna') if parti==1 else urlEncodeNonAscii(part.encode('utf-8'))
+        for parti, part in enumerate(parts)
+    )
+
 def fixEntities(text):
     '''This function replaces special characters with &entities; '''
     if not text:
@@ -82,14 +93,28 @@ def fixEntities(text):
     if not isinstance(text, unicode):
         text = text.decode('latin1')
     result = ''
-    for c in text:
-        if not (c in ('<', '>', '/', '"', "'", '=', '&')):
-            if c.encode("latin1", 'replace') in entitydefs2.keys():
-                rep = entitydefs2[c.encode("latin1", "replace")]
-                rep = '&'+rep+';'
-                result += rep
+    #FIXME: we don't handle the case when the url contains an escaped quote (\").
+    exclude_re = re.compile('''(< *img.*?src=['"]|< *a.*?href=['"]|< *link.*?href=['"])(.*?)['"]''', re.IGNORECASE)
+    while True:
+        match = exclude_re.search(text)
+        if match:
+            text2 = text[:match.start(2)]
+        else:
+            text2 = text
+
+        for c in text2:
+            if not (c in ('<', '>', '/', '"', "'", '=', '&')):
+                if c.encode("latin1", 'replace') in entitydefs2.keys():
+                    rep = entitydefs2[c.encode("latin1", "replace")]
+                    rep = '&'+rep+';'
+                    result += rep
+                else:
+                    result += c
             else:
                 result += c
+        if match:
+            result += iriToUri(match.group(2))
+            text = text[match.end(2):]
         else:
-            result += c
+            break
     return result
